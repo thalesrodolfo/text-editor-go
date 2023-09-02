ff4cd83e99e5ce536805548ba328066417b54960
Thales Rodolfo <thalesrodolfo@protonmail.com>
2023-09-02 14:03:43 -0300

    feat: backspace implemented

--- main.go
+++ main.go
@@ -2,27 +2,26 @@ package main
 
 import (
 	"fmt"
-	"strings"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
-var msg strings.Builder
+//var msg strings.Builder
 
 type Editor struct {
 	buffer []string
-	line int
+	line   int
 	cursor rl.Vector2
 }
 
-
-
 func (e *Editor) addChar(c int32, font rl.Font) {
-	e.buffer = append(e.buffer, fmt.Sprintf("%c", c))
+	fmt.Println(e.buffer[e.line])
+	e.buffer[e.line] = e.buffer[e.line] + fmt.Sprintf("%c", c)
 	e.cursor.X += font.Recs.Width
 }
 
 func (e *Editor) removeChar(font rl.Font) {
+	e.buffer[e.line] = e.buffer[e.line][0 : len(e.buffer[e.line])-1]
 	e.cursor.X -= font.Recs.Width
 }
 
@@ -41,6 +40,19 @@ func (e Editor) String() {
 	fmt.Println()
 }
 
+func blinkCursor(blink *float64, cursorColor *rl.Color) {
+	if *blink >= 0.5 {
+
+		if cursorColor.A == 0.0 {
+			cursorColor.A = 200
+		} else {
+			cursorColor.A = 0.0
+		}
+
+		*blink = 0.0
+	}
+}
+
 func stopBlink(blink *float64, color *rl.Color) {
 	*blink = 0.0
 	*color = rl.NewColor(60, 60, 60, 200)
@@ -51,6 +63,7 @@ func main() {
 	screenHeight := int32(450)
 
 	editor := new(Editor)
+	editor.buffer = append(editor.buffer, "")
 
 	rl.InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window")
 
@@ -63,57 +76,45 @@ func main() {
 
 	editor.cursor = fontPosition
 
-	fmt.Println(font.Recs.Width)
-
 	blink := 0.0
 	cursorColor := rl.NewColor(60, 60, 60, 200)
 
 	for !rl.WindowShouldClose() {
 		blink += float64(rl.GetFrameTime())
 
-
 		k := rl.GetCharPressed()
 		if k > 0 {
-			msg.WriteString(fmt.Sprintf("%c", k))
+			//msg.WriteString(fmt.Sprintf("%c", k))
 			editor.addChar(k, font)
 			stopBlink(&blink, &cursorColor)
-			editor.String()
+			//editor.String()
+			fmt.Println(editor.buffer[0])
 		}
 
 		if rl.IsKeyPressed(rl.KeyEnter) {
-			msg.WriteString("\n")
+			//msg.WriteString("\n")
 		}
 
 		if rl.IsKeyPressed(rl.KeyBackspace) {
 
-			if msg.Len() > 0 {
-				a := msg.String()[0 : msg.Len()-1]
-				msg.Reset()
-				msg.WriteString(a)
-				fmt.Println(a)
+			if len(editor.buffer[editor.line]) > 0 {
 				editor.removeChar(font)
+				stopBlink(&blink, &cursorColor)
 			}
+
 		}
 
 		rl.BeginDrawing()
 
 		rl.ClearBackground(rl.RayWhite)
 
-		fmt.Println(blink)
-		if blink >= 0.5 {
-
-			if cursorColor.A == 0.0 {
-				cursorColor.A = 200
-			} else {
-				cursorColor.A = 0.0
-			}
-
-			blink = 0.0
-		}
-
+		blinkCursor(&blink, &cursorColor)
 		rl.DrawRectangle(int32(editor.cursor.X), int32(editor.cursor.Y), fontSize/2, fontSize, cursorColor)
 
-		rl.DrawTextEx(font, msg.String(), fontPosition, float32(fontSize), 0, rl.Black)
+		for _, v := range editor.buffer {
+			// 	// draw text
+			rl.DrawTextEx(font, v, fontPosition, float32(fontSize), 0, rl.Black)
+		}
 
 		rl.EndDrawing()
 	}
